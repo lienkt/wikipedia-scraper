419a2e7992fefc1500860b909da3be5207c78807
sitarasachi <sithara.sachi@gmail.com>
2026-06-05 14:44:03 +0200

    updated api

--- src/api_client.py
+++ src/api_client.py
@@ -8,33 +8,36 @@ class APIClient:
     args:
       config (dict): A dictionary containing API configuration, including base URL and endpoints. 
     """
-    self.base_url = config["base_url"].rstrip("/")
+    self.base_url = config["base_url"]
     self.country_endpoint = config["country_endpoint"]
     self.leaders_endpoint = config["leaders_endpoint"]
     self.cookies_endpoint = config["cookies_endpoint"]
     self.session = requests.Session()
-    self.refresh_cookie()
-    
+    self.session.headers.update({
+        "User-Agent": config["user_agent"]
+    })
           
   def refresh_cookie(self) -> None:
-        """Fetch and store fresh cookies in the session."""
-        url = f"{self.base_url}{self.cookies_endpoint}"
-        response = self.session.get(url)
-        response.raise_for_status()
+    """Fetch a new cookie from the API and store it for future requests.
+    """
+    response = self.session.get(f"{self.base_url}{self.cookies_endpoint}")
+    response.raise_for_status()
 
-        # Session automatically stores cookies
-        return
+  def _ensure_cookie(self) -> None:
+      """Ensure that a valid cookie is available for API requests. If not, refresh the cookie."""
+      if not self.session.cookies:
+          self.refresh_cookie()
 
   def get_countries(self) -> list:
     """Fetch the list of countries from the API.
       Returns:
         list: A list of country names obtained from the API.
       """
-    self.refresh_cookie()
+    self._ensure_cookie()
 
     response = self.session.get(
-            f"{self.base_url}{self.country_endpoint}"
-        )
+        f"{self.base_url}{self.country_endpoint}"
+    )
     response.raise_for_status()
     return response.json()
 
@@ -45,11 +48,11 @@ class APIClient:
     Returns:
       list: A list of leaders for the specified country obtained from the API.
     """
-    self.refresh_cookie()
+    self._ensure_cookie()
 
     response = self.session.get(
-    f"{self.base_url}{self.leaders_endpoint}",
-            params={"country": country}
-        )
+        f"{self.base_url}{self.leaders_endpoint}",
+        params={"country": country}
+    )
     response.raise_for_status()
     return response.json()
